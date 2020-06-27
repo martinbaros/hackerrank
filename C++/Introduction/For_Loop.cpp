a7294f11422b0e28afc11f16fa0899fc440ded7e
Martin <mb.martinbaros@gmail.com>
2020-06-27 09:03:23 +0200

    Update Day 1

---
+++ C++/Introduction/For_Loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a,b;
+    cin >> a >> b;
+    string nums[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    for(int i = a; i<=b; i++){
+        if (i <= 9){
+            cout << nums[i-1] << endl;
+        }else if(i%2 == 0){
+            cout << "even" << endl;
+        }else{
+            cout << "odd" << endl;
+        }
+    };
+    return 0;
+}
