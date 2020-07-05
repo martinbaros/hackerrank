382902952130647f64d84c2764fef3937edcd750
Martin <mb.martinbaros@gmail.com>
2020-07-05 12:44:40 +0200

    Deque

--- C++/STL/Print_Pretty.cpp
+++ C++/STL/Print_Pretty.cpp
@@ -12,7 +12,7 @@ int main() {
 		double C; cin >> C;
         cout << std::hex << std::left << std::showbase << std::nouppercase;
         cout << (long long) A << endl;
-        cout << std::dec << std::right << std::setw(15) << std::setfill('_') <<         std::showpos << std::fixed << std::setprecision(2);
+        cout << std::dec << std::right << std::setw(15) << std::setfill('_') <<std::showpos << std::fixed << std::setprecision(2);
         cout << B << endl;
         cout << std::scientific << std::uppercase << std::noshowpos << std::setprecision(9);
         cout << C << endl;
